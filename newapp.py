e508927ce6cfda8ba54fdcf72a81e72738ebb0e1
azamhizbul <azamhizbul@gmail.com>
2022-05-24 16:15:49 +0700

    update

--- newapp.py
+++ newapp.py
@@ -1,13 +1,8 @@
 from datetime import timedelta
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api, reqparse
-from security import authenticate, identity
+from flask import Flask
+from flask_restful import Api
 from account import Account
 from userAuth import UserAuth
-
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
@@ -18,31 +13,10 @@ app.config["JWT_SECRET_KEY"] = "super-secret"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
 jwt = JWTManager(app)
 
-
-# @app.route("/login", methods=["POST"])
-# def login():
-#     username = request.json.get("username", None)
-#     password = request.json.get("password", None)
-#     if username != "test" or password != "test":
-#         return jsonify({"msg": "Bad username or password"}), 401
-
-#     access_token = create_access_token(identity=username)
-#     return jsonify(access_token=access_token)
-
-
-# Protect a route with jwt_required, which will kick out requests
-# without a valid JWT present.
-# @app.route("/protected", methods=["GET"])
-# @jwt_required()
-# def protected():
-#     # Access the identity of the current user with get_jwt_identity
-#     current_user = get_jwt_identity()
-#     return jsonify(logged_in_as=current_user), 200
-
 api.add_resource(Account, '/accounts')
 api.add_resource(UserAuth, '/login')
+
 if __name__ == "__main__":
     app.run()
 
 app.run(port=5000, debug=True)
-
